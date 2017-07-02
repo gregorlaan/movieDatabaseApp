7b39e8a541159a53b2357e164185d19c8469deaf
Gregor Laan <gregor@github.ee>
2017-07-02 20:30:00 +0300

    Get latest movies data

--- src/components/LatestMovies.vue
+++ src/components/LatestMovies.vue
@@ -3,3 +3,26 @@
       Latest Movies
   </div>
 </template>
+
+<script>
+export default {
+
+    methods: {
+        getData: function() {
+            this.$http({
+            url: 'https://api.themoviedb.org/3/movie/upcoming?api_key=a648c1e2f660e4faa6619ab957405dcf', 
+            method: 'GET'
+            }).then(function (response) {
+                // success callback
+                var data = response.body;
+                console.log(data);
+
+            }, function (response) {
+                // error callback
+            });
+        }
+    }
+
+}
+</script>
+
