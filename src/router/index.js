34f12b1b99538e8be284157113f2539f4ecfe353
Gregor Laan <gregor@github.ee>
2017-07-02 20:24:55 +0300

    Created and imported LatestMovies component

--- src/router/index.js
+++ src/router/index.js
@@ -1,6 +1,7 @@
 import Vue from 'vue';
 import Router from 'vue-router';
 import Home from '@/components/Home';
+import LatestMovies from '@/components/LatestMovies';
 
 Vue.use(Router);
 
@@ -11,6 +12,11 @@ export default new Router({
       path: '/',
       name: 'Home',
       component: Home
+    },
+    {
+      path: '/latest',
+      name: 'LatestMovies',
+      component: LatestMovies
     }
   ]
 })
